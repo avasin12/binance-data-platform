c7edc243a86c82f96b4f086aca044cc63b397d9b
avasin12 <alexmain2013@ya.ru>
2026-08-03 12:09:09 +0000

    Dag_params validation

--- dags/acme/utils/dag_params.py
+++ dags/acme/utils/dag_params.py
@@ -1,3 +1,22 @@
 # /home/avasin/airflow/dags/acme/utils/dag_params.py
 
-DEFAULT_BINANCE_PARAMS = {"symbol": "BTCUSDT", "limit": 100}
+
+from airflow.sdk import Param
+
+DEFAULT_BINANCE_PARAMS = {
+    "symbol": Param(
+        title="Symbol",
+        default="BTCUSDT",
+        type="string",
+        minLength=3,
+        maxLength=30,
+        pattern="^[A-Z0-9]+$",
+    ),
+    "limit": Param(
+        title="Maximum trades per API page",
+        default=1000,
+        type="integer",
+        minimum=1,
+        maximum=1000,
+    ),
+}
